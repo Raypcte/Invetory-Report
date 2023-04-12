dc8281b99364ec4b69498db2af7e325b6078ad3c
Raypcte <rayanevalak@gmail.com>
2023-04-11 23:47:42 -0300

    req08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Baton",
+        "Doce Sabor Ltda",
+        "01-10-2021",
+        "01-10-2023",
+        "123",
+        "em local fresco e seco"
+    )
+
+    assert str(product) == (
+        'O produto Baton'
+        ' fabricado em 01-10-2021'
+        ' por Doce Sabor Ltda com validade'
+        ' até 01-10-2023'
+        ' precisa ser armazenado em local fresco e seco.'
+    )
